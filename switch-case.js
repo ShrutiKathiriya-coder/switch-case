bd5ddbbe8642e73669ce02b021483e6f9644ff3b
ShrutiKathiriya-coder <kathiriyashruti3@gmail.com>
2024-11-27 19:07:44 +0530

    Add files via upload

---
+++ switch-case.js
@@ -0,0 +1,19 @@
+var num1=30;
+var num2=20;
+var choice=add;
+switch (choice) {
+    case "add":
+            console.log("addition :"+(num1+num2));
+        break;
+        case "sub":
+            console.log("substract :"+(num1-num2));
+        break;
+        case "mul":
+            console.log("multipliction :"+(num1*num2));
+        break;
+        case "add":
+            console.log("sub :"+(num1/num2));
+        break;
+    default:
+        break;
+}
